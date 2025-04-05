4238c509e9e7da2f5351f954c4ff523dd846250e
VirtualPirate <artaza13020artaza@gmail.com>
2025-04-05 23:40:03 +0530

    Made the contact us page responsive

--- src/components/home-page/contact-us.tsx
+++ src/components/home-page/contact-us.tsx
@@ -6,20 +6,20 @@ const ContactUs = () => {
       className="flex flex-col justify-center items-center mt-20 md:mt-28 lg:mt-32 xl:mt-36 gap-16 sm:gap-20 md:gap-24 lg:gap-28 xl:gap-32 mb-16"
       id="contact-us"
     >
-      <div className="flex w-full justify-between">
-        <div className="w-[65%] p-24 flex flex-col gap-24">
+      <div className="flex flex-col lg:flex-row w-full justify-between">
+        <div className="w-full lg:w-[69%] p-6 sm:p-12 md:p-16 lg:p-24 flex flex-col gap-8 sm:gap-12 md:gap-16 lg:gap-24">
           <div>
-            <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-left">
+            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-left">
               CONTACT US !
             </h2>
-            <div className="text-lg">
+            <div className="text-base sm:text-lg">
               Send us your requirements and talk to us
             </div>
           </div>
 
-          <div className=" flex flex-col gap-10">
+          <div className="flex flex-col gap-6 sm:gap-8 md:gap-10">
             <div>
-              <h3 className="text-lg font-medium text-white mb-2">
+              <h3 className="text-base sm:text-lg font-medium text-white mb-2">
                 Company / Business
               </h3>
               <input
@@ -28,9 +28,11 @@ const ContactUs = () => {
                 className="w-full py-2 bg-transparent border-b border-white text-white placeholder-white focus:outline-none"
               />
             </div>
-            <div className="flex w-full gap-10">
+            <div className="flex flex-col lg:flex-row w-full gap-6 sm:gap-10">
               <div className="w-full">
-                <h3 className="text-lg font-medium text-white mb-2">Email</h3>
+                <h3 className="text-base sm:text-lg font-medium text-white mb-2">
+                  Email
+                </h3>
                 <input
                   type="text"
                   placeholder=""
@@ -38,7 +40,7 @@ const ContactUs = () => {
                 />
               </div>
               <div className="w-full">
-                <h3 className="text-lg font-medium text-white mb-2">
+                <h3 className="text-base sm:text-lg font-medium text-white mb-2">
                   Phone Number
                 </h3>
                 <input
@@ -49,7 +51,9 @@ const ContactUs = () => {
               </div>
             </div>
             <div>
-              <h3 className="text-lg font-medium text-white mb-2">Message</h3>
+              <h3 className="text-base sm:text-lg font-medium text-white mb-2">
+                Message
+              </h3>
               <input
                 type="text"
                 placeholder=""
@@ -62,7 +66,7 @@ const ContactUs = () => {
             Send Message
           </PrimaryButton>
         </div>
-        <div className="hidden sm:block">
+        <div className="hidden lg:block">
           <img src="/fractal.png" alt="Fractal" className="object-contain" />
         </div>
       </div>
